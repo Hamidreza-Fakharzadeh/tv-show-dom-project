3e88ee29baf73a9e236c6542563a0974c177d6b4
hamidreza <hrfarzadeh@gmail.com>
2020-05-14 03:25:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,6 @@ function cardHeader(episode) {
     }
     return 'S' + numSeason + 'E' + numEpisode + ' ' + name;
 }
-let idShow = 82;
 fetch('https://api.tvmaze.com/shows/82/episodes')
     .then(response => response.json())
     .then(dataJson => {
@@ -74,101 +73,6 @@ fetch('https://api.tvmaze.com/shows/82/episodes')
         selectBox(dataJson);
     });
 
-//     const rootElem = document.getElementById('root');
-//     //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 
-//     //LIST ELEMENT
-//     function listEpisodes() {
-//         const selectTag2 = document.getElementById('selectTag2');
-//         for (let i = 0; i < episodeList.length; i++) {
-//             let optionElm = document.createElement('option');
-//             selectTag2.appendChild(optionElm);
-//             optionElm.innerText =
-//                 'S0' +
-//                 episodeList[i].season +
-//                 'E0' +
-//                 episodeList[i].number +
-//                 ' ' +
-//                 '-' +
-//                 ' ' +
-//                 episodeList[i].name;
-//         }
-//     }
-//     listEpisodes();
-
-//     //DISPLY NUMBER OF MOVIES LABLE
-//     function displyNumberOfMovies() {
-//         let numApi = episodeList.length;
-//         const lableTag = document.getElementById('lableTag');
-//         lableTag.innerHTML = numApi + '/' + ' ' + numApi + ' ' + 'Episodes';
-//     }
-//     displyNumberOfMovies();
-
-//     //SEARCH ELEMENT
-//     // function searchEpisode() {
-//     //     const inputTag = document.getElementById('inputTag');
-//     // }
-//     //DISPLAY MOVIES
-//     function displayMovies() {
-//         const optionCtn = document.getElementById('optionCtn');
-//         for (let i = 0; i < episodeList.length; i++) {
-//             let divElem = document.createElement('div');
-//             divElem.className = 'col-12 sm-col-12 md-col-6 lg-col-4 xl-col-4';
-//             optionCtn.appendChild(divElem);
-//             const cardFrame = document.createElement('div');
-//             divElem.appendChild(cardFrame);
-//             cardFrame.className = 'eachCard';
-//             let h1Elem = document.createElement('h1');
-//             let h1Append = cardFrame.appendChild(h1Elem);
-//             h1Elem.className = 'h1Class';
-//             let imgElem = document.createElement('img');
-//             let imgAppend = cardFrame.appendChild(imgElem);
-//             let paragraphElem = document.createElement('p');
-//             let paragraphAppend = cardFrame.appendChild(paragraphElem);
-//             h1Elem.textContent =
-//                 episodeList[i].name +
-//                 ' ' +
-//                 '-' +
-//                 ' ' +
-//                 'S0' +
-//                 episodeList[i].season +
-//                 'E0' +
-//                 episodeList[i].number;
-
-//             imgElem.src = episodeList[i].image.medium;
-
-//             let eleSummary = episodeList[i].summary;
-//             let peice = eleSummary.replace('<p>', ' ');
-//             summaryElem = peice.replace('</p>', ' ');
-//             paragraphElem.textContent = summaryElem;
-//         }
-//     }
-//     displayMovies();
-
-//     function clickMovie(e) {
-//         let term = e.target.value.toUpperCase();
-//         let lableSearch = document.getElementById('lableTag');
-//         const movies = document.getElementsByClassName('eachCard');
-//         let arrSearch = [];
-//         Array.from(movies).forEach(function(movie) {
-//             let titel = movie.firstElementChild.textContent;
-//             if (titel.toUpperCase().indexOf(term) != -1) {
-//                 arrSearch.push(titel);
-//                 let lenthSearch = arrSearch.length;
-//                 movie.style.display = 'block';
-//                 lableSearch.value =
-//                     lenthSearch + ' ' + '/' + ' ' + numEpi + ' ' + 'Episodes';
-//             } else {
-//                 movie.style.display = 'none';
-//             }
-//         });
-//     }
-
-//     function inputListenerCall() {
-//         let getNodInput = document.getElementById('inputTag');
-//         getNodInput.addEventListener('keyup', clickMovie);
-//     }
-//     inputListenerCall();
-// }
 
 window.onload = setup();
